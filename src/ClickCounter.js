6ae07c9c36451f19d562e1e7a258e6fee8bee77f
bocai <xiaobo.huang@hand-china.com>
2017-07-23 16:20:26 +0800

    more content

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -8,13 +8,40 @@ class ClickCounter extends Component {
 	     this.onClickMinusButton = this.onClickMinusButton.bind(this);
 		this.state = { count: props.initValue }
 	}
+	componentWillMount(){
+		console.log("enter componentWillMount:" + this.props.caption)
+	}
+	componentDidMount(){
+		console.log("enter componentDidMount:" + this.props.caption)
+	}
 	onClickPlusButton(){
-		this.setState({ count: this.state.count + 1});
+		// this.setState({ count: this.state.count + 1});
+		this.updateCount(true);
 	}
 	onClickMinusButton(){
-		this.setState({ count: this.state.count - 1})
+		// this.setState({ count: this.state.count - 1})
+		this.updateCount(false);
+	}
+	componentWillReceiveProps(nextProps){
+		console.log("enter componentWillReceiveProps:" + this.props.caption)
+	}
+	shouldComponentUpdate(nextProps, nextState){
+       if(nextProps.caption !== this.props.caption){
+       	return true;
+       }
+       if(nextState.count !== this.state.count){
+       	return true;
+       }
+       return false;
+	}
+	updateCount(isIncrement){
+		const previousValue = this.state.count;
+		const newValue = isIncrement ? previousValue + 1 : previousValue -1;
+		this.setState({count: newValue});
+		this.props.onUpdate(newValue, previousValue);
 	}
 	render(){
+		console.log("enter componentRender:" + this.props.caption)
 		const caption = this.props.caption
 		const countStyle = {
 			margin: '30px'
@@ -36,10 +63,12 @@ class ClickCounter extends Component {
 
 ClickCounter.propTypes = {
 		caption: PropTypes.string.isRequired,
-		initValue: PropTypes.number
+		initValue: PropTypes.number,
+		onUpdate: PropTypes.func
 	}
 //set default props
 ClickCounter.defaultProps = {
-	initValue: 0
+	initValue: 0,
+	onUpdate: f => f
 }
 export default ClickCounter;
--- src/CounterPanel.js
+++ src/CounterPanel.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import ClickCounter from './ClickCounter';
 
 class CounterPanel extends Component {
+  constructor(props){
+  	super(props);
+  	this.onCounterUpdate = this.onCounterUpdate.bind(this);
+  	this.initValues = [0, 10, 20];
+  	const initSum = this.initValues.reduce((a,b) => a + b, 0);
+  	this.state  = { sum: initSum }
+  }
+  onCounterUpdate(newValue, previousValue){
+  	const valueChange = newValue - previousValue;
+  	this.setState({sum: this.state.sum + valueChange});
+  }
   render(){
+  	console.log("enter CounterPanel render");
   	return(
             <div>
-                <ClickCounter caption="First" />
-  	           <ClickCounter caption="second" initValue={1} />
-  	           <ClickCounter caption="Third" initValue={2} />
+                <ClickCounter caption="First" onUpdate = { this.onCounterUpdate } />
+  	           <ClickCounter caption="second" initValue={ this.initValues[1] } onUpdate = { this.onCounterUpdate } />
+  	           <ClickCounter caption="Third" initValue={ this.initValues[2] } onUpdate = { this.onCounterUpdate } />
+                <hr />
+                <div>{ this.state.sum }</div>
+  	           <button onClick={ () => this.forceUpdate()}>
+                  Click me to repaint!
+  	           </button>
             </div>
   		);
   
